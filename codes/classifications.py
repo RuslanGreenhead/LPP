1713b787984d7f2c8cb67f5e7bfe1a2ada0c9e3c
Ruslan <ruslan15-mol@mail.ru>
2022-08-24 11:02:53 +0300

    classification pipeline finished

--- codes/classifications.py
+++ codes/classifications.py
@@ -1,10 +1,55 @@
 # import flow as nf
-# import tf_neural_networks as tf_nn
+import tf_neural_networks as tf_nn
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.gaussian_process import GaussianProcessClassifier
 from sklearn.gaussian_process.kernels import DotProduct, WhiteKernel
+
+
+def apply_a_classifier(alg_name, n_estimators, x_train, y_train):
+
+    alg_name = alg_name.lower()
+
+    # Random Forest Regressor
+    if alg_name == "rfc":
+        print("Random Forest!")
+
+        model = RandomForestClassifier(n_estimators=n_estimators,
+                                       criterion='gini',
+                                       min_samples_leaf=1,
+                                       verbose=1,
+                                       n_jobs=-2,
+                                       )
+
+    # Gaussian Process Regressor
+    elif alg_name == "gpc":
+        print("Gaussian Process!")
+        kernel = DotProduct() + WhiteKernel()
+        model = GaussianProcessClassifier(kernel=kernel,)
+
+    # Gradient Boosting Regressor
+    elif alg_name == "gbc":   # does not support 2d y
+        print("Gradient Boosting!")
+        model = GradientBoostingClassifier(n_estimators=n_estimators,
+                                           verbose=1,
+                                           loss='deviance',
+                                           )
+
+    # Adaboost Regressor
+    elif alg_name == "ac":  # does not support 2d y
+        print("Adaboost!")
+        model = AdaBoostClassifier(n_estimators=n_estimators,)
+
+    else:
+        print ("Undefined classification model")
+        # f = True
+        # assert f is True
+        quit()
+
+    model.fit(x_train, y_train)
+
+    return model
 
 
 def instantiate_fit_cls_model(alg_name, loss, n_units, input_dim,
@@ -18,13 +63,6 @@ def instantiate_fit_cls_model(alg_name, loss, n_units, input_dim,
     elif alg_name.lower() == "dnn_cls":
         loss_fn = tf_nn.determine_cls_tf_loss(loss=loss)
         _model = tf_nn.DNNClassification(n_units=n_units, input_dim=input_dim, output_dim=output_dim)
-
-    elif alg_name.lower() == "nf_cls":
-        model = nf.NFFitter(var_size=output_dim, cond_size=input_dim, batch_size=batch_size,
-                            n_epochs=n_epochs, lr=learning_rate)
-        model.fit(x_train, y_train)
-
-        history = None
 
     elif alg_name.lower() == "rfc" or alg_name.lower() == "gbc" or \
             alg_name.lower() == "ac":
@@ -44,9 +82,10 @@ def instantiate_fit_cls_model(alg_name, loss, n_units, input_dim,
     else:
         _model = None
         history = None
-        print("Undefined model.")
-        f = True
-        assert f is True
+        print("Undefined model")
+        # f = True
+        # assert f is True
+        quit()
 
     if alg_name.lower() == "vnn_cls" or alg_name.lower() == "dnn_cls":
         model, history = tf_nn.compile_and_fit(model=_model, optimizer=optimizer,
@@ -56,3 +95,4 @@ def instantiate_fit_cls_model(alg_name, loss, n_units, input_dim,
                                                )
 
     return model, history
+
